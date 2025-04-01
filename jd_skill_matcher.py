2fc1043ac70ec59e5545644469a4a751a5983fc0
vivienanhnguyen <vivienanh@gmail.com>
2025-04-01 13:32:00 +0700

    Update jd_skill_matcher.py

--- jd_skill_matcher.py
+++ jd_skill_matcher.py
@@ -120,7 +120,14 @@ def merge_all_skills(matched, missing, extra):
 
 
 # Load spaCy model
-nlp = spacy.load("en_core_web_sm")
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 # --- Sidebar Uploads ---
 st.sidebar.header("📂 Upload Files")
